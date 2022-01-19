cc8830c2c3f70d3bfc50c1af8931d4cf92a2701f
hwisun <yhs333@gmail.com>
2022-01-19 13:47:54 +0900

    first time

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-const express = require('express')
+const express = require('express');
 const app = express()
 const port = 5000
+const config = require('./config/key');
+const mongoose = require('mongoose');
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://hudsonyoo:akfls753@boilerplate.n8fak.mongodb.net/myFirstDatabase?retryWrites=true&w=majority').then(() => console.log('MongoDB Connected..')).catch(err => console.log(err))
 
-app.get('/',(req, res) => res.send('Hello World!!!안녕'))
+mongoose.connect(config.mongoURI).then(() => console.log('MongoDB Connected..')).catch(err => console.log(err))
+
+
+const {User} = require("./models/User");
+const bodyParser = require("body-Parser");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+//application/json
+app.use(bodyParser.json());
+
+
+app.get('/',(req, res) => res.send('Hello World'))
+
+app.post('/register',(req, res) => {
+
+    //회원가입할때 필요한 정보들을 client에서 가져오면 db에 넣어준다.
+
+
+    const user = new User(req.body);
+
+    user.save((err,userInfo) => {
+        if(err) return res.json({success:false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
---
+++ models/config/dev.js
@@ -0,0 +1,3 @@
+module.exports={
+    mongoURI:'mongodb+srv://hudsonyoo:akfls753@boilerplate.n8fak.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
